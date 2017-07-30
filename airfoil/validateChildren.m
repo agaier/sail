318eb8cb9808b2b4b063aaacef8a3763312a8a70
agaier <adam.ga@gmail.com>
2017-07-30 21:22:18 +0200

    clean up

--- airfoil/validateChildren.m
+++
@@ -1,7 +0,0 @@
-function validity = validateChildren(children,p)    
-
-[~, ul, ll, parsecParams] = p.express(children);
-for i=1:p.nChildren
-    % TODO: Vectorize getValidity
-    validity(i) = getValidity(ul(:,:,i),ll(:,:,i),parsecParams(i,:)); %#ok<AGROW>
-end
